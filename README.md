ab1348b27ee798cda82d266812995edd8baec1b0
test <you@example.com>
2018-11-10 14:42:43 +0800

    use the gulp to start a website and compile the sass automatically

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # wolfwolf
 Only a study project for web
+
+## Day one
+- Study the gulp
+TODO:
+    - [x] Use the gulp to start a website and compile the sass automatically
+    >  **Unable to compile file name with underlined file**
+    
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+let gulp = require('gulp');
+let browserSync = require('browser-sync').create();
+let sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+gulp.task('serve', ['sass'], function () {
+    
+    browserSync.init({
+        server: './'
+    });
+
+    gulp.watch('./source/*.sass', ['sass']);
+    gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+    return gulp.src('./source/*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('./css/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+
+// Can't output *.css when sass file has _ in the head
+gulp.task('scss', function(){
+    return gulp.src('./source/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('scss:watch', function() {
+    gulp.watch('./source/*.scss', ['scss']);
+});
